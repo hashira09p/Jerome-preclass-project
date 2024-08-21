f0edacf2ad78764e7a41a3f33e49a0ea7fa0d38c
Jerome <jeromeabelida19@gmail.com>
2024-08-21 14:19:55 +0800

    return the fullname of the hash value

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,13 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+name = {first_name: "Jerome", middle_name: "Dela Cruz", last_name: "Abe" }
+def fullName(name)
+  theName =""
+  name.each do |name, value|
+    theName += "#{value}  "
+  end
+  return theName
+end
+
+print fullName(name)
