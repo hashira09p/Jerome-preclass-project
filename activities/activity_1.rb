910f3e5d9e72b0234f65bf5ee3567c03a7ea784a
Jerome <jeromeabelida19@gmail.com>
2024-08-21 14:05:01 +0800

    activity 1: Fuction that return the avarage age of adults

--- activities/activity_1.rb
+++ activities/activity_1.rb
@@ -2,3 +2,16 @@
 #  Write a function that takes an Array of ages,
 #  the function will return the average age of adults.
 #  Print on console the result.
+age =[3, 23, 42, 6, 2, 16, 78, 21, 23]
+def checkingAges(age)
+  total =0
+  age.each do |number|
+    if(18 <= number)
+      total += number
+    end
+  end
+  return total
+end
+
+print checkingAges(age)
+
